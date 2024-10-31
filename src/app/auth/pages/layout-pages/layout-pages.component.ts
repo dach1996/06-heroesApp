2f26e85f8ccd92db6c84b94d466722e7d3e9b4e4
Danny Cárdenas <danny.cardenas@bayteq.com>
2024-10-31 14:25:29 -0500

    cap 104

---
+++ src/app/auth/pages/layout-pages/layout-pages.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-pages',
+  templateUrl: './layout-pages.component.html',
+})
+export class LayoutPagesComponent {
+
+}
