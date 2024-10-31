6f661cb6a78bdb449345198c84ed22cb6da85522
Danny Cárdenas <danny.cardenas@bayteq.com>
2024-10-31 14:50:29 -0500

    cap 186

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth.routing.module';
+import { LayoutPagesComponent } from './pages/layout-pages/layout-pages.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LayoutPagesComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
