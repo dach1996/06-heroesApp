b91eb4d03dce5366a78e96313b9474b1f4d8bee4
Danny Cárdenas <danny.cardenas@bayteq.com>
2024-10-31 16:56:29 -0500

    cap 194

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -7,6 +7,7 @@ import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { MaterialModule } from '../material/material.module';
 
 
 @NgModule({
@@ -15,11 +16,12 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
     HeroPageComponent,
     ListPageComponent,
     NewPageComponent,
-    SearchPageComponent
+    SearchPageComponent,
   ],
   imports: [
     CommonModule,
-    HeroesRoutingModule
+    HeroesRoutingModule,
+    MaterialModule
   ]
 })
 export class HeroesModule { }
